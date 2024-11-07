045fc91bed37d2f4969d77f2a709deaacf4abe18
Chamod420 <chamoddilshan0907@gmail.com>
2024-11-07 21:46:31 +0530

    fixses logo size

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -94,7 +94,7 @@ const HorizontalScrollCarousel = () => {
 
               {/* Paragraph */}
               <div className=" md:px-10 xl:w-1/2">
-                <img src="/Group 1.png" alt="Terracode" className="w-[200px] sm:w-[800px]"/>
+                <img src="/Group 1.png" alt="Terracode" className="w-[200px] sm:w-[400px] md:w-[500px] lg:w-[600px] xl:w-[800px]"/>
               </div>
               </motion.div>
         
