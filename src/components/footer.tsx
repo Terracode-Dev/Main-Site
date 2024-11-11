ad7144e1e466fd1d4c9d174c1e92960e5e63deda
Chamod420 <chamoddilshan0907@gmail.com>
2024-11-11 11:37:56 +0530

    build issuses done

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -23,16 +23,16 @@ const Footer = () => {
           <div className="mb-4 space-y-4">
             <ul className="space-y-2">
               <li >
-                <Link to={"/aboutus"} className="text-gray-300 transition-colors hover:text-orange-400">go to the aboutus page</Link>
+                <Link to={"/aboutus"} className="text-gray-300 transition-colors hover:text-orange-400">about us </Link>
               </li>
               <li >
-                <Link to={"/casestudy"} className="text-gray-300 transition-colors hover:text-orange-400">go to the casestudies page</Link>
+                <Link to={"/casestudy"} className="text-gray-300 transition-colors hover:text-orange-400">case studies </Link>
               </li>
               <li >
-                <Link to={"/aboutus"} className="text-gray-300 transition-colors hover:text-orange-400">go to the aboutus page</Link>
+                <Link to={"/aboutus"} className="text-gray-300 transition-colors hover:text-orange-400">about us </Link>
               </li>
               <li >
-                <Link to={"/casestudy"} className="text-gray-300 transition-colors hover:text-orange-400">go to the casestudies page</Link>
+                <Link to={"/casestudy"} className="text-gray-300 transition-colors hover:text-orange-400">case studies </Link>
               </li>
             </ul>
           </div>
